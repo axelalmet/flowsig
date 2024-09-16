505e405623e134df1f353f13e93b129f2f58e100
axelalmet <axelalmet@gmail.com>
2024-09-16 11:54:00 -0400

    Updating version as some changes have been implemented.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowsig',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     description='Use graphical modeling to infer communication-driven intercellular flows from single-cell RNA-sequencing and spatial transcriptomics data.',
     url='https://github.com/axelalmet/flowsig',

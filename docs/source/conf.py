aebc46b6576f1d645dd2ac34e4e044a1e716e179
axelalmet <axelalmet@gmail.com>
2026-02-09 17:16:22 -0500

    Still trying to fix documentation building.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,9 +74,9 @@ myst_heading_anchors = 3
 
 # Intersphinx links (optional but useful)
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", {}),
-    "numpy": ("https://numpy.org/doc/stable", {}),
-    "scipy": ("https://docs.scipy.org/doc/scipy", {}),
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy", None),
 }
 
 # -- Options for HTML output -------------------------------------------------

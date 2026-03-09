38bde181d87ed763d383a694567f85bd0d90f6dd
axelalmet <axelalmet@gmail.com>
2026-03-08 23:10:59 -0700

    Updating __init__.py

--- src/flowsig/__init__.py
+++ src/flowsig/__init__.py
@@ -2,3 +2,7 @@ from importlib.metadata import version as _version
 
 __all__ = ["__version__"]
 __version__ = _version("flowsig")
+
+from . import preprocessing as pp
+from . import tools as tl
+from . import plotting as pl
